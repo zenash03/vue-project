aa6a61ecd1ca530387e7f6b96c5f6bc100e9822d
Wyrtzen Wensley <wyrtzen.ecc@gmail.com>
2022-02-24 13:56:52 +0700

    Components

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,14 @@
 <template>
-<input type="text" v-model="cari" placeholder="cari.."/>
-<div v-if="products.length">
-  <ul>
-    <li v-for="item in cariData" :key="item">
-      {{ item.title }} -- {{ item.price }}
-    </li>
-  </ul>
-</div>
-<div v-else>
-  Data Not Found
-</div>
+  <Header/>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
 
 export default {
   name: 'App',
-  data() {
-    return {
-      cari : "",
-      products: []
-    }
-  },
-  created() {
-    this.products = [
-        {id : 1, title : 'Product 1', price : 2000},
-        {id : 2, title : 'Product 2', price : 3000},
-        {id : 3, title : 'Product 3', price : 4000},
-        {id : 4, title : 'Product 4', price : 1000},
-        {id : 5, title : 'Product 5', price : 5000},
-      ]
-  },
-  computed: {
-    cariData() {
-      return this.products.filter((item) => {
-        return item.title.match(this.cari)
-      })
-    }
+  components: {
+    Header
   }
 }
 </script>
@@ -46,7 +18,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
