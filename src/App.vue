ba6ec015632f39503590d63cb791fcb98d9bfed7
Wyrtzen Wensley <wyrtzen.ecc@gmail.com>
2022-02-24 13:44:46 +0700

    Conditional n Loops

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <input type="text" v-model="name" />
-  <br />
-  <h2>{{ name }}</h2>
+<div v-if="products.length">
+  <ul>
+    <li v-for="item in products" :key="item">
+      {{ item.title }} -- {{ item.price }}
+    </li>
+  </ul>
+</div>
+<div v-else>
+  Data Not Found
+</div>
 </template>
 
 <script>
@@ -10,7 +17,13 @@ export default {
   name: 'App',
   data() {
     return {
-      name : ""
+      products: [
+        {id : 1, title : 'Product 1', price : 2000},
+        {id : 2, title : 'Product 2', price : 3000},
+        {id : 3, title : 'Product 3', price : 4000},
+        {id : 4, title : 'Product 4', price : 1000},
+        {id : 5, title : 'Product 5', price : 5000},
+      ]
     }
   }
 }
